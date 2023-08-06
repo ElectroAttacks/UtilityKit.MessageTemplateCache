4602bb6da32da8a52b95ae8f32a5d9559c772f2b
ElectroAttacks <casparjacobweber@gmail.com>
2023-08-06 21:24:04 +0200

    #Added XML Documentary

--- MessageTemplateCache.Analyzers/Resources/DiagnosticRules.cs
+++ MessageTemplateCache.Analyzers/Resources/DiagnosticRules.cs
@@ -2,12 +2,21 @@
 
 namespace MessageTemplateCache.Analyzers.Resources;
 
+/// <summary>
+/// Contains static diagnostic rules for MessageTemplateCache analyzers.
+/// </summary>
 internal static class DiagnosticRules
 {
 
+    /// <summary>
+    /// The URI of the help link for the MessageTemplateCache analyzers.
+    /// </summary>
     public const string _helpLinkUri = "https://github.com/ElectroAttacks/UtilityKit.MessageTemplateCache/wiki/Help";
 
 
+    /// <summary>
+    /// Represents the diagnostic rule for invalid arguments.
+    /// </summary>
     public static readonly DiagnosticDescriptor InvalidArgumentRule = new(
         id: "MTC0001",
         title: GetLocalizedString(nameof(DiagnosticMessages.InvalidArgument_Title)),
@@ -19,6 +28,9 @@ internal static class DiagnosticRules
         helpLinkUri: _helpLinkUri
         );
 
+    /// <summary>
+    /// Represents the diagnostic rule for missing identifiers.
+    /// </summary>
     public static readonly DiagnosticDescriptor MissingIdentifierRule = new(
         id: "MTC0002",
         title: GetLocalizedString(nameof(DiagnosticMessages.MissingIdentifier_Title)),
@@ -31,7 +43,11 @@ internal static class DiagnosticRules
         );
 
 
-
+    /// <summary>
+    /// Retrieves the localized string for the given resource key.
+    /// </summary>
+    /// <param name="resourceKey">The key of the resource string.</param>
+    /// <returns>The localized string for the given resource key.</returns>
     public static LocalizableString GetLocalizedString(string resourceKey)
        => new LocalizableResourceString(resourceKey, DiagnosticMessages.ResourceManager, typeof(DiagnosticMessages));
 
